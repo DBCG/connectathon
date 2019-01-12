4037079cdd3b9f8b661002712fa7ca81c9173c15
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-01-11 18:17:46 -0700

    More work on the scripts

--- scripts/getpatientreports.sh
+++ scripts/getpatientreports.sh
@@ -4,14 +4,14 @@ MEASURE=${1:-measure-col}
 YEAR=${2:-2017}
 BASE_URL=${3:-http://localhost:8080/cqf-ruler/baseDstu3}
 
-file=./results/$MEASURE-$YEAR.json
+file=../results/$MEASURE-$YEAR.json
 if [ ! -f $file ]
 then
 	echo "$file not found. Exiting"
     exit
 fi
 
-directory=./results/$MEASURE-$YEAR-patient
+directory=../results/$MEASURE-$YEAR-patient
 if [ -d $directory ]
 then
     rm -r -f $directory
@@ -34,7 +34,7 @@ echo "Denominator $DENOMINATOR_ID"
 
 
 function get_patient_ids() {
-    jq ".contained[] | select(.id==\"$1\") | .entry[].item.reference" $file |  tr -d '"' | cut -d/ -f3
+    jq -r ".contained[] | select(.id==\"$1\") | .entry[].item.reference" $file  | cut -d/ -f2
 }
 
 function get_patient_info() {
@@ -42,14 +42,11 @@ function get_patient_info() {
     | jq '{resourceType : "Patient", id, name : ((.name[0].given | join(" ")) + " " + (.name[0].family)), gender, birthDate}'
 }
 
-
-CQL=$(cat col-logic.cql)
-
 function get_measure_result() {
     curl -sS "$BASE_URL/Measure/$MEASURE/\$evaluate-measure?patient=$1&periodStart=$YEAR-01-01&periodEnd=$YEAR-12-31&_format=json" \
     -H "Accept: application/json" \
     -H "Content-Type:application/json" \
-    | jq '.' #jq -C '.[] | select(.resultType=="Boolean") | { name, result }'
+    | jq '.'
 }
 
 # Only need the initial population for now. We'll run the cql for each patient directly to capture the output.
@@ -57,10 +54,12 @@ IDS=($(get_patient_ids $POPULATION_ID))
 for i in ${IDS[@]}
 do
     echo "Getting Patient Info $i"
-    echo "[" >> $directory/$i.json
-	echo $(get_patient_info $i) >> $directory/$i.json
-    echo "," >> $directory/$i.json
+    echo "{ \"patient\" : " >> temp.json
+	echo $(get_patient_info $i) >> temp.json
     echo "Getting Measure Report $i"
-    echo $(get_measure_result $i) >> $directory/$i.json
-    echo "]" >> $directory/$i.json
+    echo ", \"report\" : " >> temp.json
+    echo $(get_measure_result $i) >> temp.json
+    echo "}" >> temp.json
+    cat temp.json | jq -M '.' > $directory/$i.json
+    rm temp.json
 done
--- scripts/runmeasure.sh
+++ scripts/runmeasure.sh
@@ -4,12 +4,20 @@ MEASURE=${1:-measure-col}
 YEAR=${2:-2017}
 BASE_URL=${3:-http://localhost:8080/cqf-ruler/baseDstu3}
 
+results=../results
+if [ -d $results] 
+then
+    rm -r -f $results
+else
+    mkdir $results
+fi
+
 
 echo "Evaluating $MEASURE for $YEAR on $BASE_URL"
 curl "$BASE_URL/Measure/$MEASURE/\$evaluate-measure?reportType=patient-list&periodStart=$YEAR-01-01&periodEnd=$YEAR-12-31&_format=json" \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--o "./results/$MEASURE-$YEAR.json"
+-o "$results/$MEASURE-$YEAR.json"
 
 
 # http://localhost:8080/cqf-ruler/baseDstu3/Measure/measure-col/$evaluate-measure?reportType=patient-list&periodStart=2017-01-01&periodEnd=2017-12-31&_format=json
