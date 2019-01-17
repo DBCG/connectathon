c24685d80bd6fa6180441e64901886914dffe51d
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-01-17 13:32:30 -0700

    Updated output for latest cqf version

--- scripts/createoutput.sh
+++ scripts/createoutput.sh
@@ -23,11 +23,10 @@ function get_population_status() {
 
 function output() {
     jq -r '.patient | "\(.id)\t\(.name)\t\(.gender)\t\(.birthDate)"' $1
-    echo $MEASURE$'\t''Initial Population'$'\t'$(get_population_status "initial-population" $1)
-    echo $MEASURE$'\t''Numerator'$'\t'$(get_population_status "numerator" $1)
-    echo $MEASURE$'\t''Denominator'$'\t'$(get_population_status "denominator" $1)
-    jq -r 'select(.report.contained !=null) | .report.contained[0].entry[] | select(.resource.resourceType=="Observation") | "\(.resource.resourceType)\t\(.resource.id)\t\(.resource.effectiveDateTime)\t\(.resource.code.coding[0].system)\t\(.resource.code.coding[0].code)\t\(.resource.category[0].coding[0].display)"' $1
-    jq -r 'select(.report.contained !=null) | .report.contained[0].entry[] | select(.resource.resourceType=="DiagnosticReport") | "\(.resource.resourceType)\t\(.resource.id)\t\(.resource.issued)\t\(.resource.code.coding[0].system)\t\(.resource.code.coding[0].code)\t\(.resource.code.coding[0].display)"' $1
+    echo 'Initial Population'$'\t'$(get_population_status "initial-population" $1)
+    echo 'Denominator'$'\t'$(get_population_status "denominator" $1)
+    echo 'Numerator'$'\t'$(get_population_status "numerator" $1)
+    jq -r '{ mapping: [(.report.contained[0].entry[] | select(.resource.resourceType=="List") | .resource | { title, resource: (.entry[].item.reference | split("/")[1])})], resources: [select(.report.contained !=null) | .report.contained[0].entry[] | select(.resource.resourceType!="List") | .resource? | { id, type:(.resourceType), system :(.code.coding[0].system), code: (.code.coding[0].code), display: (.code.coding[0].display), date: (.effectiveDateTime // .issued // .performedPeriod.start) } ] } | JOIN(INDEX((.mapping[]); .resource); (.resources[]); .id) | "\(.[1].title)\t\(.[0].type)\t\(.[0].id)\t\(.[0].system)\t\(.[0].code)\t\(.[0].date)\t"' $1
     echo $'\n'
 }
 
