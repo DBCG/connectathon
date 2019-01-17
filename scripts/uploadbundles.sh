479f8df4214396032f2d6df1570bd2ce43f5be13
Jonathan Percival <jonathan.i.percival@gmail.com>
2019-01-17 13:59:58 -0700

    Change base url

--- scripts/uploadbundles.sh
+++ scripts/uploadbundles.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-BASE_URL=${1:-http://measure.eval.kanvix.com/cqf-ruler/baseDstu3}
+BASE_URL=${1:-http://localhost:8080/cqf-ruler/baseDstu3}
 
-# echo 'Loading FHIR Helpers'
-# curl "$BASE_URL/Library/FHIRHelpers" \
-# -H "Accept: application/json" \
-# -H "Content-Type:application/json" \
-# -X PUT \
-# --data @<(cat ../bundles/FhirHelpers_v3.0.0-library.json)
+echo 'Loading FHIR Helpers'
+curl "$BASE_URL/Library/FHIRHelpers" \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-X PUT \
+--data @<(cat ../bundles/FhirHelpers_v3.0.0-library.json)
 
 # echo 'Posting All CQM Bundle'
 # curl "$BASE_URL" \
@@ -58,23 +58,23 @@ BASE_URL=${1:-http://measure.eval.kanvix.com/cqf-ruler/baseDstu3}
 
 
 # Local bundles
-echo 'Posting Hedis terminology bundle'
-curl "$BASE_URL" \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--X POST \
---data @<(cat ../../cqf-ruler/src/test/resources/org/opencds/cqf/measure-evaluation/hedis-terminology-bundle.json)
+# echo 'Posting Hedis terminology bundle'
+# curl "$BASE_URL" \
+# -H "Accept: application/json" \
+# -H "Content-Type:application/json" \
+# -X POST \
+# --data @<(cat ../../cqf-ruler/src/test/resources/org/opencds/cqf/measure-evaluation/hedis-terminology-bundle.json)
 
-echo 'Posting Hedis Practioner ValueSet'
-curl "$BASE_URL/ValueSet/hedis-med-rec-practitioner" \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--X PUT \
---data @<(cat ../../hedis-ig/resources/valueset/valueset-hedis-med-rec-practitioner.json)
+# echo 'Posting Hedis Practioner ValueSet'
+# curl "$BASE_URL/ValueSet/hedis-med-rec-practitioner" \
+# -H "Accept: application/json" \
+# -H "Content-Type:application/json" \
+# -X PUT \
+# --data @<(cat ../../hedis-ig/resources/valueset/valueset-hedis-med-rec-practitioner.json)
 
-echo 'Posting QDM terminology bundle'
-curl "$BASE_URL" \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--X POST \
---data @<(cat ../../cqf-ruler/src/test/resources/org/opencds/cqf/qdm-data-provider/qdm-terminology-bundle.json)
+# echo 'Posting QDM terminology bundle'
+# curl "$BASE_URL" \
+# -H "Accept: application/json" \
+# -H "Content-Type:application/json" \
+# -X POST \
+# --data @<(cat ../../cqf-ruler/src/test/resources/org/opencds/cqf/qdm-data-provider/qdm-terminology-bundle.json)
