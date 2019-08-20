f6d3cf78a85e815e84035e26d7cbf27eca88d0c1
Adam Stevenson <stevenson_adam@yahoo.com>
2019-08-20 15:19:37 -0600

    Update uploadvaluesets.sh

--- scripts/uploadvaluesets.sh
+++ scripts/uploadvaluesets.sh
@@ -16,21 +16,6 @@ do
 echo 'Cleaning ' "$f";
 JSON=$(cat "$f" \
 | jq --arg old "http://www.ama-assn.org/go/cpt" --arg new "CPT" "$JQ_PROGRAM" \
-| jq --arg old "http://hl7.org/fhir/sid/icd-9-cm" --arg new "ICD-9-CM" "$JQ_PROGRAM" \
-| jq --arg old "http://hl7.org/fhir/sid/icd-10-us" --arg new "ICD-10-CM" "$JQ_PROGRAM" \
-| jq --arg old "http://www.icd10data.com/icd10pcs" --arg new "ICD-10-PCS" "$JQ_PROGRAM" \
-| jq --arg old "http://www.ama-assn.org/go/hcpcs" --arg new "HCPCS" "$JQ_PROGRAM" \
-| jq --arg old "https://www.cms.gov/Medicare/Coding/MedHCPCSGenInfo/index.html" --arg new "HCPCS" "$JQ_PROGRAM" \
-| jq --arg old "http://snomed.info/sct" --arg new "SNOMED CT US Edition" "$JQ_PROGRAM" \
-| jq --arg old "http://loinc.org" --arg new "LOINC" "$JQ_PROGRAM" \
-| jq --arg old "http://www.nubc.org/patient-discharge" --arg new "UBREV" "$JQ_PROGRAM" \
-| jq --arg old "http://hl7.org/fhir/claimcareteamrole" --arg new "http://hl7.org/fhir/claimcareteamrole" "$JQ_PROGRAM" \
-| jq --arg old "http://www.nlm.nih.gov/research/umls/rxnorm" --arg new "RXNORM" "$JQ_PROGRAM" \
-| jq --arg old "CMS Specialty" --arg new "CMS Specialty" "$JQ_PROGRAM" \
-| jq --arg old "http://nucc.org/provider-taxonomy" --arg new "Provider Taxonomy" "$JQ_PROGRAM" \
-| jq --arg old "http://hl7.org/fhir/sid/cvx" --arg new "CVX" "$JQ_PROGRAM" \
-| jq --arg old "http://terminology.hl7.org/fhir/CodeSystem/medicationdispense-category" --arg new "NDC HEDIS 2019" "$JQ_PROGRAM" \
-| jq --arg old "http://hl7.org/fhir/sid/ndc" --arg new "NDC" "$JQ_PROGRAM" \
 )
 
 echo 'Posting' "$f";
