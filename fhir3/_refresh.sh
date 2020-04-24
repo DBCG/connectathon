6f1c52ecff9bfa7a8d4ed4b863180c233eb59d43
Adam Stevenson <stevenson_adam@yahoo.com>
2020-04-24 16:44:43 -0600

    Updated refresh scripts for 1.1.0 tooling and to pass IG Resource path.

--- fhir3/_refresh.sh
+++ fhir3/_refresh.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-1.0.4-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
+ig_resource_path=./input/connectathon.xml
+
 set -e
 echo Checking internet connection...
 wget -q --spider tx.fhir.org
@@ -18,13 +20,13 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -ip="$PWD" -iv=fhir3 -t -d -p -v $fsoption
+	JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -iv=fhir3 -t -d -p -v $fsoption
 
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -iv=fhir3 -t -d -p -v $fsoption
+		JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -igrp="$ig_resource_path" -iv=fhir3 -t -d -p -v $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateRefreshIG.  Aborting...
 	fi
