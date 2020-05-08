75ccfc835cdb702c892e9d1ad5637c833a71a45c
rob-reynolds <4z&ChMu6yGn%IL>
2020-05-07 22:52:58 -0600

    Version updates for 104 Use latest cqf tooling

--- fhir401/_refresh.bat
+++ fhir401/_refresh.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET tooling_jar=tooling-1.0.4-SNAPSHOT-jar-with-dependencies.jar
+SET tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
 
 ECHO Checking internet connection...
@@ -17,10 +17,10 @@ SET fsoption=-fs http://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
+	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v
 	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
 ) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
+	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v
 	JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateRefreshIG.  Aborting...
